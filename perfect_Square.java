9646c65a991fcc43516eaa7b5b29095bda42120c
Amit Ghatage <117560391+AmitGhatage001@users.noreply.github.com>
2022-12-24 19:19:17 +0530

    perfect Square optimized TC-O(N/2)
    Given a number A. Return 1 if it is perfect square otherwise return 0.

---
+++ perfect_Square.java
@@ -0,0 +1,36 @@
+package others;
+
+
+    import java.lang.*;
+import java.util.*;
+
+    public class perfect_Square{
+        public static void main(String[] args) {
+            // YOUR CODE GOES HERE
+            // Please take input and print output to standard input/output (stdin/stdout)
+            // DO NOT USE ARGUMENTS FOR INPUTS
+            // E.g. 'Scanner' for input & 'System.out' for output
+            Scanner obj = new Scanner(System.in);
+            int T=obj.nextInt();
+
+            for(int j=0;j<T;j++){
+
+                long N=obj.nextInt();
+                int flag=0;
+                if(N==1){
+                    System.out.println(1);
+                }
+
+                else{
+                    for(long i=1;i<=N/2;i++){
+                        if(i*i==N){
+                            flag=1;
+                            break;
+                        }
+                    }
+                    System.out.println(flag);
+                }
+            }
+        }
+    }
+
