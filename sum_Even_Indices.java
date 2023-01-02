454ac66130a1031f44a40b75967947b0ace90980
Amit Ghatage <117560391+AmitGhatage001@users.noreply.github.com>
2023-01-02 20:52:20 +0530

    Sum of even indices
    You are given an array A of length N and Q queries given by the 2D array B of size Q*2. Each query consists of two integers B[i][0] and B[i][1].
    For every query, the task is to calculate the sum of all even indices in the range A[B[i][0]…B[i][1]].
    
    Note : Use 0-based indexing

---
+++ sum_Even_Indices.java
@@ -0,0 +1,41 @@
+package DSA_Array_two;
+import java.util.*;
+public class sum_Even_Indices {
+
+    public ArrayList<Integer> solve(ArrayList<Integer> A, ArrayList<ArrayList<Integer>> B) {
+
+        // Creating prefix ArrayList
+        // Solved by my own logic felt little proud today
+        ArrayList<Integer> Prefix = new ArrayList<>();
+        int Sum=0;
+        for(int i=0;i<A.size();i++){
+            if(i==0 || i%2==0){
+                Sum = Sum + A.get(i);
+                Prefix.add(Sum);
+            }
+            else{
+                Prefix.add(Sum);
+            }
+
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0;i<B.size();i++){
+            int l=B.get(i).get(0);
+            int r=B.get(i).get(1);
+
+            int ans=0;
+
+            if(l==0){
+                ans=Prefix.get(r);
+            }
+            else{
+                ans=Prefix.get(r) - Prefix.get(l-1);
+            }
+            list.add(ans);
+
+
+        }
+        return list;
+    }
+
+}
