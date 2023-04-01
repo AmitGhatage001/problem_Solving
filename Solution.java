e739833d9e93480cc635090ebebdf574dbdc22f5
Amit Ghatage <117560391+AmitGhatage001@users.noreply.github.com>
2023-04-02 00:30:48 +0530

    Print Linked List
    You are given A which is the head of a linked list. Print the linked list in space separated manner.
    
    Note : The last node value must also be succeeded by a space and after printing the entire list you should print a new line

--- Solution.java
+++ Solution.java
@@ -1,21 +1,20 @@
-package Kadans_algorithm;
+package Linked_List;
 import java.util.*;
 public class Solution {
-    public int maxSubArray(final List<Integer> A) {
+    public void solve(ListNode A) {
 
-        int total=0;
-        int maxSum=Integer.MIN_VALUE;
 
-        for(int i=0;i<A.size();i++){
-            total = total + A.get(i);
-            if(total>maxSum){
-                maxSum=total;
-            }
-            if(total<0){
-                total=0;
-            }
+
+        while(A!=null){
+            System.out.print(A.val+" ");
+            A=A.next;
         }
+        System.out.println();
+        // if(temp.next==null){
+        //     System.out.print(temp.val+" ");
+        // }
+        // System.out.print(temp.val+" ");
 
-        return maxSum;
+
     }
 }
