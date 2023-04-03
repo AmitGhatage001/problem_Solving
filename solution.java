79e6fd18556ffb28ff5aa62fd2040b74a2dc4e5d
Amit Ghatage <117560391+AmitGhatage001@users.noreply.github.com>
2023-04-03 23:23:54 +0530

    Print Linked List
    You are given A which is the head of a linked list. Print the linked list in space separated manner.
    
    Note : The last node value must also be succeeded by a space and after printing the entire list you should print a new line

---
+++ solution.java
@@ -0,0 +1,20 @@
+package linked_list;
+
+public class solution {
+    public void solve(ListNode A) {
+
+
+
+        while(A!=null){
+            System.out.print(A.val+" ");
+            A=A.next;
+        }
+        System.out.println();
+        // if(temp.next==null){
+        //     System.out.print(temp.val+" ");
+        // }
+        // System.out.print(temp.val+" ");
+
+
+    }
+}
