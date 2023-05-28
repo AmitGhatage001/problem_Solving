6be56e789381d65bd3206d44922b299e843305e3
Amit Ghatage <117560391+AmitGhatage001@users.noreply.github.com>
2023-05-28 19:39:54 +0530

    Fractional Knapsack
    Problem Description
    Given two integer arrays A and B of size N each which represent values and weights associated with N items respectively.
    
    Also given an integer C which represents knapsack capacity.
    
    Find out the maximum total value that we can fit in the knapsack. If the maximum total value is ans, then return ⌊ans × 100⌋ , i.e., floor of (ans × 100).
    
    NOTE:
    
    You can break an item for maximizing the total value of the knapsack
    
    
    Problem Constraints
    1 <= N <= 105
    
    1 <= A[i], B[i] <= 103
    
    1 <= C <= 103
    
    
    
    Input Format
    First argument is an integer array A of size N denoting the values on N items.
    
    Second argument is an integer array B of size N denoting the weights on N items.
    
    Third argument is an integer C denoting the knapsack capacity.
    
    
    
    Output Format
    Return a single integer denoting the maximum total value of A such that sum of the weights of this subset is smaller than or equal to C.
    
    
    
    Example Input
    Input 1:
    
     A = [60, 100, 120]
     B = [10, 20, 30]
     C = 50
    Input 2:
    
     A = [10, 20, 30, 40]
     B = [12, 13, 15, 19]
     C = 10
    
    
    Example Output
    Output 1:
    
     24000
    Output 2:
    
     2105
    
    
    Example Explanation
    Explanation 1:
    
    Taking the full items with weight 10 and 20 and 2/3 of the item with weight 30 will give us 
    the maximum value i.e 60 + 100 + 80 = 240. So we return 24000.
    Explanation 2:
    
    Taking 10/19 the fourth item gives us the maximum value i.e. 21.0526. So we return 2105.

--- Solution.java
+++ Solution.java
@@ -1,20 +1,81 @@
-package Linked_List;
+package Greedy_algo;
+import java.util.Comparator;
 import java.util.*;
 public class Solution {
-    public void solve(ListNode A) {
+    // here our ans was double(in decimal) and when we multiply by 100 it was modifying my answer to a diffrent value
+    // for example ans is 2.55 for testcase A[3] , B[20] and C=17
+    // But for above test case it returns 254
+    // bcz when 2.55 * 100 = Computer gives 254.99999999999997
+    // and our answer if modified from 255 to 254 when { return (int)ans } return statement is executed in code
+    // So to tackle this loss we initially multiply the number by 1000 {so 2.55 becomes 2550}
+    // and then divide {2550} by 10 to get correct answer which in 255 {(2.55*1000)/10}
+
+    // these condition we encounter in cases where we typecast double/float values in integer by multiplying with multiple of 10 i.e (100 or 1000)
+    // to avoid the ans being modified to a diffrent value we first multiply by the multiple of 10 which add a extra zero to our decimal value and convert ot to Integer
+    // and then divide by 10
+    // for example 2.55 we convert to 2550 by multiplying by 1000 and at last divide by 10 to get accurate answer
+    // this way we avoide getting wrong answer which because of computer returning double value i.e rounded value which when converted to integer gives wrong answer
 
 
+    // Learn Comparator function to sort arrays
 
-        while(A!=null){
-            System.out.print(A.val+" ");
-            A=A.next;
-        }
-        System.out.println();
-        // if(temp.next==null){
-        //     System.out.print(temp.val+" ");
-        // }
-        // System.out.print(temp.val+" ");
-
+    // class attributes/properties
+    int weight;
+    int value;
+    double value_per_weight;
+    //getters
+    public int getWeight(){
+        return weight;
+    }
+    public int getValue(){
+        return value;
+    }
+    public double getValue_per_weight(){
+        return value_per_weight;
+    }
+    // Constructor
+    Solution(int value,int weight,double value_per_weight){
+        this.value=value;
+        this.weight=weight;
+        this.value_per_weight=value_per_weight;
+    }
+    // we need default constructor with parameterized consytructor here
+    Solution(){
 
     }
+    public int solve(int[] A, int[] B, int C) {
+        // created array of objects of a class Solution
+        Solution[] arr = new Solution[A.length];
+        // Created objects and stored in the array
+        for(int i=0;i<A.length;i++){
+            int value=A[i];
+            int weight=B[i];
+            double value_per_weight=(double)A[i]/B[i];
+            arr[i]= new Solution(value,weight,value_per_weight);
+        }
+        // sort the array based on Value_per_weight
+        // sorting the array based on Value_per_weight attribute
+        Arrays.sort(arr,Comparator.comparingDouble(Solution::getValue_per_weight));
+
+        // Main Logic
+        double ans=0;
+        for(int i=arr.length-1;i>=0;i--){
+            if(arr[i].weight<=C){
+                ans=ans+arr[i].value;
+                C=C-arr[i].weight;
+            }
+            else{
+                ans=ans + C*arr[i].value_per_weight;
+                break;
+            }
+        }
+        // System.out.println(ans);
+
+        // System.out.println(ans*100);
+
+        return (int)((ans*1000)/10);
+
+        // return (int)(ans*100);
+    }
 }
+
